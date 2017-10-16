82008146ed3288b3ca6cf7a7fa313416e3fc13d3
BI4HUU <BI4HUU@yandex.ru>
2017-10-16 15:15:13 +0400

    Add files via upload
    Vse v JS

--- script.js
+++ script.js
@@ -1,10 +1,71 @@
+let buttonData = [
+	[
+		[`input`],[`inputSlide`],[`button`],[`onclick`, `fS1()`],[`<`],[``]
+	],
+	[
+		[`div`],[``],[``],[``, ``],[``],[`wIN`]
+	],
+	[
+		[`input`],[`inputSlide`, ],[`button`],[`onclick`, `nextAuto()`],[`>`],[``]
+	]
+];
+let buttonData2 = [
+	[
+		[`input`],[`fullScreenButton`],[``],[`onclick`, `toggleFullScreen()`],[`< >`]
+	],
+	[
+		[`input`],[`inputSlideBigLeft inputSlideBig`],[`button`],[`onclick`, `fS1()`],[`<`]
+	],
+	[
+		[`input`],[`inputSlideBigPause inputSlideBig`],[`button`],[`onclick`, `pause()`],[`| |`]
+	],
+	[
+		[`input`],[`inputSlideBigRight inputSlideBig`],[`button`],[`onclick`, `nextAuto()`],[`>`]
+	],
+	[
+		[`div`],[`wrapperInputSlide`, ],[``],[``, ``],[``]
+	]
+];
+let controlPanelBlock = document.createElement(`div`);
+	controlPanelBlock.className = `controlPanel`;
+	document.getElementsByClassName('wrapperSpanSlide')[0].appendChild(controlPanelBlock);
+
+for (let i = 0; i < buttonData2.length; i++) {
+	element = document.createElement(buttonData2[i][0]);
+	element.className = `${buttonData2[i][1]}`;
+	element.type = `${buttonData2[i][2]}`;
+	// element.setAttribute(buttonData[i][3][0],buttonData[i][3][1]);
+	element.value = `${buttonData2[i][4]}`;
+	// document.getElementsByClassName('wrapperInputSlide')[0].appendChild(element);
+	// WrapFullScreen
+		document.getElementsByClassName('controlPanel')[0].appendChild(element)
+}
+
+for (let i = 0; i < buttonData.length; i++) {
+	element = document.createElement(buttonData[i][0]);
+	element.className = `${buttonData[i][1]}`;
+	element.type = `${buttonData[i][2]}`;
+	// element.setAttribute(buttonData[i][3][0],buttonData[i][3][1]);
+	element.value = `${buttonData[i][4]}`;
+	element.id = `${buttonData[i][5]}`;
+	document.getElementsByClassName('wrapperInputSlide')[0].appendChild(element);
+
+}
+document.getElementsByClassName(`inputSlide`)[0].setAttribute(buttonData[0][3][0],buttonData[0][3][1]);
+document.getElementsByClassName(`inputSlide`)[1].setAttribute(buttonData[2][3][0],buttonData[2][3][1]);
+document.getElementsByClassName(`fullScreenButton`)[0].setAttribute(buttonData2[0][3][0],buttonData2[0][3][1]);
+document.getElementsByClassName(`inputSlideBigLeft`)[0].setAttribute(buttonData2[1][3][0],buttonData2[1][3][1]);
+document.getElementsByClassName(`inputSlideBigPause`)[0].setAttribute(buttonData2[2][3][0],buttonData2[2][3][1]);
+document.getElementsByClassName(`inputSlideBigRight`)[0].setAttribute(buttonData2[3][3][0],buttonData2[3][3][1]);
+console.log(document.getElementsByClassName('wrapperInputSlide')[0].appendChild(element));
 
 let time = 8000; // Пауза між кадрами (мілісек)
 let speed = 1;  // скорость листання (сек)
 function Id(e) {return document.getElementById(e)};
 function CE(e) {return document.createElement(e)};
-let WrapFullScreen = Id("WrapFullScreen");
+// let WrapFullScreen = Id("WrapFullScreen");
 let quantity = document.getElementsByClassName('wrapperSpanSlide')[0].childNodes;
+let WrapFullScreen = document.getElementsByClassName('wrapperSpanSlide')[0];
 let quantityButton = Id('wIN').childNodes;
 let quantityLength = quantity.length-1; // Кількість слайдів
 // let numberNextSlide = quantityLength;  // Номер наступного слайда
@@ -22,7 +83,6 @@ for (let i = 0; i < quantityLength; i++) {
 		input.style.background = `url("${i+1}.jpg") `;
 		input.style.backgroundSize = `100% 100%`;
 		// input.value=`${i+1}`;
-		// input.value=node.cloneNode(deep);
 		input.setAttribute('onclick', `fS1(${i+1})`);
 	Id('wIN').appendChild(input);
 };
@@ -110,7 +170,168 @@ document.addEventListener('keydown', function(event) {
     if (event.keyCode==32) {pause()};
         }, false);
 
-// let test = document.createElement('style');
-// test.innerHTML = '.fullScreenButton{background: rgba(256, 88, 88, 0.9)}';
+let style = document.createElement('style');
+style.innerHTML = `body {
+  background-color: #888;
+  padding: 0;
+  margin: 0;
+  width: 100%; }
+.wrapperSpanSlide {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background-color: #333;
+  padding: 0;
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto; }
+  .wrapperSpanSlide span {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-size: cover; }
 
-// WrapFullScreen.appendChild(test);
+.controlPanel {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0 auto;
+  position: absolute;
+  z-index: 8888;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0; }
+
+.fullScreenButton {
+  position: absolute;
+  z-index: 8888;
+  top: 0;
+  right: 0;
+  height: 15%;
+  width: 11%;
+  border-radius: 0 0 0 30%;
+  font: 55px GOST Type BU;
+  box-sizing: border-box;
+  padding: 0.2em 0.4em 0.2em 0.4em;
+  opacity: 0.08;
+  color: #444;
+  background: rgba(88, 88, 88, 0.2);
+  border: 1px rgba(88, 88, 88, 0.2) solid;
+  text-shadow: 0 0 2px rgba(250, 250, 250, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center; }
+  .fullScreenButton:hover {
+    opacity: 1; }
+
+.inputSlideBig {
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+  height: 70%;
+  width: 30%;
+  font: 188px GOST Type BU;
+  color: #444;
+  background: rgba(88, 88, 88, 0.2);
+  border: 1px rgba(88, 88, 88, 0.2) solid;
+  text-shadow: 0 0 2px rgba(250, 250, 250, 0.5);
+  opacity: 0.08; }
+  .inputSlideBig:hover {
+    opacity: 1; }
+
+.inputSlideBigLeft {
+  left: 0;
+  border-top-right-radius: 40%;
+  border-bottom-right-radius: 40%; }
+
+.inputSlideBigRight {
+  right: 0;
+  border-top-left-radius: 40%;
+  border-bottom-left-radius: 40%; }
+
+.inputSlideBigPause {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 50%;
+  width: 25%;
+  border-radius: 40%; }
+
+.wrapperInputSlide {
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  z-index: 8888;
+  opacity: 0.1; }
+  .wrapperInputSlide:hover {
+    opacity: 1; }
+
+.inputSlide {
+  background: rgba(88, 88, 88, 0.3);
+  padding: 1vh 7vh;
+  border: 0;
+  color: #444;
+  font: 38px GOST Type BU; }
+  .inputSlide:hover {
+    background: rgba(88, 88, 88, 0.5);
+    border: 1px rgba(88, 88, 88, 0.2) solid;
+    text-shadow: 0 0 1px rgba(250, 250, 250, 0.6); }
+
+#wIN {
+  display: flex;
+  flex-wrap: no-wrap; }
+
+#slide1 {
+  background-image: url("1.jpg"); }
+
+#slide2 {
+  background-image: url("2.jpg"); }
+
+#slide3 {
+  background-image: url("3.jpg"); }
+
+#slide4 {
+  background-image: url("4.jpg"); }
+
+#slide5 {
+  background-image: url("5.jpg"); }
+
+#slide6 {
+  background-image: url("6.jpg"); }
+
+#slide7 {
+  background-image: url("7.jpg"); }
+
+#slide8 {
+  background-image: url("8.jpg"); }
+
+#slide9 {
+  background-image: url("9.jpg"); }
+
+#slide10 {
+  background-image: url("10.jpg"); }
+
+#slide11 {
+  background-image: url("11.jpg"); }
+
+#slide12 {
+  background-image: url("12.jpg"); }
+
+#slide13 {
+  background-image: url("13.jpg"); }
+
+#slide14 {
+  background-image: url("14.jpg"); }
+
+#slide15 {
+  background-image: url("15.jpg"); }
+
+#slide16 {
+  background-image: url("16.jpg"); }
+`;
+
+WrapFullScreen.appendChild(style);
