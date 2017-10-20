965a52e6ed046489bee16066dd3d9eb7d8dd84ad
BI4HUU <BI4HUU@yandex.ru>
2017-10-20 14:24:15 +0300

    Add files via upload

--- ss.js
+++ ss.js
@@ -3,13 +3,13 @@ var fs = new require("fs");
 var s = http.createServer();
 s.on('request', function(request, response) {
     response.writeHead(200);
-    console.log(request.method);
-    console.log(request.headers);
-    console.log(request.url);
-    console.log(`ip: ${request.ip}`);
-    response.write(fs.readFileSync("h.html", "utf8"));
+    // console.log(request.method);
+    // console.log(request.headers);
+    // console.log(request.url);
+    // console.log(`ip: ${request.ip}`);
+    response.write(fs.readFileSync("index.html", "utf8"));
     response.end();
 });
  
 s.listen(8888);
-console.log('Browse to http://127.0.0.1:');
+console.log(`Browse to          http://127.0.0.1:8888`);
